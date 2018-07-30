952954161e04f998cfb1024bc81207c8eb970431
Almendra <estrada.almendra@gmail.com>
2018-07-30 16:37:30 -0400

    bubble

--- bubblesort.js
+++ bubblesort.js
@@ -7,33 +7,40 @@ function swap(index1, index2, arr) {
 }
 
 function compare(value1, value2) {
-  return value1 > value2;
+ console.log("compare Call");
+ return value1 > value2;
+
 }
 
 function bubbleSort(array) {
   let arr = array;
   if (arr.length < 2) {
-    console.log(arr);
     return arr;
   } else {
+    let order = false;
     let firstCompared = 0;
-    let lastCompared = arr.length + 1;
+    let lastCompared = 0;
+    do
+    {
+      order = false;
+      for (let firstCompared = 0; firstCompared < arr.length-1; firstCompared++) {
 
-    while (lastCompared > 0) {
-      for (let firstCompared = 0; firstCompared < lastCompared; firstCompared++) {
         let value1 = arr[firstCompared];
         let value2 = arr[firstCompared + 1];
         if (compare(value1, value2)) {
           arr = swap(firstCompared, firstCompared + 1, arr);
+          order = true;
         }
-        lastCompared--;
-    }
+        console.log(arr);
     }
   }
+    while(order);
+    //lastCompared++;
+    }
+   // while (lastCompared < arr.length && order);
+
+
   console.log(arr);
   return arr;
-
-
-
 
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,25 +1,90 @@
-/*describe('spy', function() {
-  beforeEach(function() {
-    swap(value1, value2, arr) = {
+describe('spy', function() {
+    beforeAll(function (){
+      spyOn(window, "swap").and.callThrough();
+      spyOn(window, "compare").and.callThrough();
+    });
 
-      }
-    };
+    afterEach(function()
+    {
+       window.swap.calls.reset();
+       window.compare.calls.reset();
+    });
+
+
   it("tracks that the spy was called", function() {
-    expect(swap).toHaveBeenCalled(6);
+    bubbleSort([3, 2, 5, 4, 1]);
+    expect(window.swap.calls.count()).toEqual(6);
+    window.swap.calls.reset();
+    window.compare.calls.reset();
   });
-});*/
+
+
+  let  singleItemArray  = [1];
+  let multipleItemArray = [3, 2, 5, 4, 1];
+  let secArray = [1,2];
+
+
+  it('when sorting an array with one item, swap is called only once', function () {
+    bubbleSort([1]);
+    expect(window.swap.calls.count()).toEqual(0);
+    window.swap.calls.reset();
+    window.compare.calls.reset();
+  });
+
+  it('when sorting an array with multiple items, swap is called the appropriate amount of times', function () {
+    window.swap.calls.reset();
+    bubbleSort(multipleItemArray);
+    expect(window.swap.calls.count()).toEqual(6);
+    window.swap.calls.reset();
+    window.compare.calls.reset();
+  });
+
+  it('when sorting an array with multiple items, comparison is called the appropriate amount of times', function () {
+    window.compare.calls.reset();
+    bubbleSort([3,5,2]);
+    expect(window.compare.calls.count()).toEqual(6);
+  });
+
+});
 
 describe('Bubble Sort', function(){
   let singleItemArray  = [1];
   let multipleItemArray = [3, 2, 5, 4, 1];
 
 beforeAll(function () {
+
+
   singleItemArray  = [1];
   multipleItemArray = [3, 2, 5, 4, 1];
+
+
+
 });
 
+// describe('compareSpy', function() {
+// afterEach(function()
+// {
+//    window.swap.calls.reset();
+//    window.compare.calls.reset();
+// });
+
+  // beforeAll(function (){
+  //   //window.swap.calls.reset();
+
+  //   spyOn(window, "compare").and.callThrough();
+
+  //   window.compare.calls.reset();
+  // });
+
+let  arrayCheck  = [1];
+let arraySize = [4,3];
+
+
+
+});
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
+
   });
 
   it('handles an array with a single item', function(){
@@ -30,20 +95,4 @@ beforeAll(function () {
     expect(  bubbleSort(multipleItemArray)).toEqual( [1, 2, 3, 4, 5] );
   });
 
-  it('when sorting an array with one item, swap is called only once', function () {
-  bubbleSort(singleItemArray);
-  expect(swap.calls.count()).toEqual(0);
-});
-
-  it('when sorting an array with multiple items, swap is called the appropriate amount of times', function () {
-  bubbleSort(singleItemArray);
-  expect(swap.calls.count(multipleItemArray)).toEqual(6);
-});
-
-  it('when sorting an array with multiple items, comparison is called the appropriate amount of times', function () {
-  bubbleSort(singleItemArray);
-  expect(compare.calls.count(multipleItemArray)).toEqual(16);
-});
-
-
-});
+//});
